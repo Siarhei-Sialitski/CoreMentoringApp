4d5f4edacadb06ac00c407bdec851efb5decae10
Siarhei Sialitski <Siarhei.Sialitski@thomsonreuters.com>
2020-10-22 18:22:44 +0300

    Add project files.

---
+++ CoreMentoringApp.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreMentoringApp.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

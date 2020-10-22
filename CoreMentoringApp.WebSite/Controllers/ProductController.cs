cd26636024cabad83b92a8a744c90233ba1de72d
Siarhei-Sialitski <nesfcc@gmail.com>
2020-10-22 20:51:33 +0300

    Task1. Base site

---
+++ CoreMentoringApp.WebSite/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using CoreMentoringApp.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMentoringApp.WebSite.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly NorthwindDbContext _dbContext;
+
+        public ProductController(NorthwindDbContext northwindDbContext)
+        {
+            _dbContext = northwindDbContext;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return View(products);
+        }
+
+    }
+}
